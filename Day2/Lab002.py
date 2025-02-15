3aafb9aa808218dcd091abd51ac9f8c156caca38
Vishal Prakash <prakashvishal93@Vishals-MacBook-Air.local>
2025-02-16 02:07:09 +0530

    added lab002

---
+++ Day2/Lab002.py
@@ -0,0 +1,7 @@
+number = int(input("Print Number : "))
+
+print(f"{number}* 1  = {number * 1}")
+print(f"{number}* 2  = {number * 2}")
+print(f"{number}* 3  = {number * 3}")
+print(f"{number}* 4  = {number * 4}")
+print(f"{number}* 5  = {number * 5}")
